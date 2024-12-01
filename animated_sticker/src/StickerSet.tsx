e53ed6c75e5c7775d89c8a2cd840180d5c60d356
Anas <anaserkinjonov@gmail.com>
2024-12-01 15:09:09 +0500

    some changes

--- animated_sticker/src/StickerSet.tsx
+++ animated_sticker/src/StickerSet.tsx
@@ -31,6 +31,8 @@ import Icon from './Icon';
 // import ReactionEmoji from './reactions/ReactionEmoji';
 import StickerButton from './StickerButton';
 
+import './StickerSet.scss';
+
 // import grey from '../../assets/icons/forumTopic/grey.svg';
 
 type OwnProps = {
